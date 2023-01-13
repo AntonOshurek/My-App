1b7c322437d76cb78d25704bde690612183ef275
Anton Oshurek <oshurekweb@gmail.com>
2023-01-13 19:54:54 +0100

    add bg image and down shadow

--- frontend/src/app/pages/root-page/root-page.tsx
+++ frontend/src/app/pages/root-page/root-page.tsx
@@ -1,7 +1,10 @@
+//styles
+import './root-page.scss';
+
 const RootPage = (): JSX.Element => {
 	return (
-		<div className="main-page">
-			<main className="">
+		<div className="root-page">
+			<main className="root-page__main">
 
 			</main>
 		</div>
