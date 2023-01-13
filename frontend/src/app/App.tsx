0d5094da2c0afe131946a4df32fffdafab967501
Anton Oshurek <oshurekweb@gmail.com>
2023-01-13 19:11:21 +0100

    create root page

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//pages
+import RootPage from './pages/root-page/root-page';
+//consts and utils functions
+import { AppRoute } from './variables/routes';
 //styles
 import './App.scss';
 
 function App() {
   return (
-		<BrowserRouter>
+		<BrowserRouter basename={AppRoute.ROOT}>
 			<Routes>
-
+				<Route path={AppRoute.ROOT} element={<RootPage/>}/>
 			</Routes>
 		</BrowserRouter>
   );
---
+++ frontend/src/app/pages/root-page/root-page.tsx
@@ -0,0 +1,11 @@
+const RootPage = (): JSX.Element => {
+	return (
+		<div className="main-page">
+			<main className="">
+
+			</main>
+		</div>
+	);
+}
+
+export default RootPage;
