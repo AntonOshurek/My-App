617b4f69875e7ba846b76e29e573229eeb4a7cea
Anton Oshurek <oshurekweb@gmail.com>
2023-01-14 11:02:18 +0100

    add clock component

--- frontend/src/app/components/header/root-header/root-header.tsx
+++ frontend/src/app/components/header/root-header/root-header.tsx
@@ -1,6 +1,7 @@
 //components
 import RootNavigation from '../../navigation/root-navigation/root-navigation';
 import RootExitButton from '../../controls/root-exit-button/root-exit-button';
+import HeaderClock from './header-clock/header-clock';
 //variables
 import { AppTitles } from '../../../variables/text';
 //styles
@@ -11,6 +12,7 @@ const RootHeader = (): JSX.Element => {
     <header className="root-header">
       <div className='root-header__wrap container'>
         <h1 className='visually-hidden'>{AppTitles.HEADER_TITLE}</h1>
+				<HeaderClock/>
         <RootNavigation/>
 				<RootExitButton/>
       </div>
