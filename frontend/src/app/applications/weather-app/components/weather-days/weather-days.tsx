34e1345d442aa771ce8ae311ab71db8f0fa4ba59
Anton Oshurek <oshurekweb@gmail.com>
2023-01-25 20:36:26 +0100

    add title for weather-days and link description

--- frontend/src/app/applications/weather-app/components/weather-days/weather-days.tsx
+++ frontend/src/app/applications/weather-app/components/weather-days/weather-days.tsx
@@ -1,45 +1,55 @@
 //styles
 import './weather-days.scss';
 
+//Monday Tuesday Wednesday Thursday Friday Saturday Sunday
+//Mon. Tue. Wed. Thu. Fri. Sat. Sun.
+
 const WeatherDays = (): JSX.Element => {
 	return (
-		<ul className='weather-days'>
-			<li className='weather-days__item weather-days__item--current'>
-				{/* Monday Tuesday Wednesday Thursday Friday Saturday Sunday */}
-				{/* Mon. Tue. Wed. Thu. Fri. Sat. Sun. */}
-				<h4 className='visually-hidden'>Monday</h4>
-				<p className='weather-days__days-name'>Mon.</p>
-				<p className='weather-days__temperature'>
-					29
-					<span className='weather-days__temperature-symbol'>°C</span>
-				</p>
-				<span className='weather-days__image'></span>
+		<>
+			<h3 className='visually-hidden'>Weather for three days</h3>
+			<ul className='weather-days'>
+				<li className='weather-days__item weather-days__item--current'>
+					<h4 className='visually-hidden'>Monday</h4>
+					<p className='weather-days__days-name'>Mon.</p>
+					<p className='weather-days__temperature'>
+						29
+						<span className='weather-days__temperature-symbol'>°C</span>
+					</p>
+					<span className='weather-days__image'></span>
 
-				<a className='weather-days__link' href="#"></a>
-			</li>
-			<li className='weather-days__item'>
-				<h4 className='visually-hidden'>Monday</h4>
-				<p className='weather-days__days-name'>Mon.</p>
-				<p className='weather-days__temperature'>
-					29
-					<span className='weather-days__temperature-symbol'>°C</span>
-				</p>
-				<span className='weather-days__image'></span>
+					<a className='weather-days__link' href="#">
+						<span className='visually-hidden'>Detailed weather for the day</span>
+					</a>
+				</li>
+				<li className='weather-days__item'>
+					<h4 className='visually-hidden'>Monday</h4>
+					<p className='weather-days__days-name'>Mon.</p>
+					<p className='weather-days__temperature'>
+						29
+						<span className='weather-days__temperature-symbol'>°C</span>
+					</p>
+					<span className='weather-days__image'></span>
 
-				<a className='weather-days__link' href="#"></a>
-			</li>
-			<li className='weather-days__item'>
-				<h4 className='visually-hidden'>Monday</h4>
-				<p className='weather-days__days-name'>Mon.</p>
-				<p className='weather-days__temperature'>
-					29
-					<span className='weather-days__temperature-symbol'>°C</span>
-				</p>
-				<span className='weather-days__image'></span>
+					<a className='weather-days__link' href="#">
+						<span className='visually-hidden'>Detailed weather for the day</span>
+					</a>
+				</li>
+				<li className='weather-days__item'>
+					<h4 className='visually-hidden'>Monday</h4>
+					<p className='weather-days__days-name'>Mon.</p>
+					<p className='weather-days__temperature'>
+						29
+						<span className='weather-days__temperature-symbol'>°C</span>
+					</p>
+					<span className='weather-days__image'></span>
 
-				<a className='weather-days__link' href="#"></a>
-			</li>
-		</ul>
+					<a className='weather-days__link' href="#">
+						<span className='visually-hidden'>Detailed weather for the day</span>
+					</a>
+				</li>
+			</ul>
+		</>
 	);
 };
 
