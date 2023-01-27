31bee5e7a98b2787d02a96d2c4e761b5c2100585
Anton Oshurek <oshurekweb@gmail.com>
2023-01-28 00:14:04 +0100

    add decoration arrow for location button

--- frontend/src/app/applications/weather-app/components/popular-locations/popular-locations.tsx
+++ frontend/src/app/applications/weather-app/components/popular-locations/popular-locations.tsx
@@ -7,16 +7,32 @@ const PopularLocaions = (): JSX.Element => {
 
 			<ul className='popular-location__list'>
 				<li className='popular-location__item'>
-					<button className='popular-location__location-button'>Warszawa</button>
+					<button className='popular-location__location-button'>Warszawa
+						<svg className='popular-location__button-icon' xmlns="http://www.w3.org/2000/svg" aria-hidden='true' width="18" height="18" viewBox="0 0 24 24">
+							<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7-7l7 7l-7 7"/>
+						</svg>
+					</button>
 				</li>
 				<li className='popular-location__item'>
-					<button className='popular-location__location-button'>Poznań</button>
+					<button className='popular-location__location-button'>Poznań
+						<svg className='popular-location__button-icon' xmlns="http://www.w3.org/2000/svg" aria-hidden='true' width="18" height="18" viewBox="0 0 24 24">
+							<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7-7l7 7l-7 7"/>
+						</svg>
+					</button>
 				</li>
 				<li className='popular-location__item'>
-					<button className='popular-location__location-button'>Białystok</button>
+					<button className='popular-location__location-button'>Białystok
+						<svg className='popular-location__button-icon' xmlns="http://www.w3.org/2000/svg" aria-hidden='true' width="18" height="18" viewBox="0 0 24 24">
+							<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7-7l7 7l-7 7"/>
+						</svg>
+					</button>
 				</li>
 				<li className='popular-location__item'>
-					<button className='popular-location__location-button'>Wrocław</button>
+					<button className='popular-location__location-button'>Wrocław
+						<svg className='popular-location__button-icon' xmlns="http://www.w3.org/2000/svg" aria-hidden='true' width="18" height="18" viewBox="0 0 24 24">
+							<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7-7l7 7l-7 7"/>
+						</svg>
+					</button>
 				</li>
 			</ul>
 		</article>
