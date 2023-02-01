a440253b1df53add3fcb8f18e1a2605098ae5fe1
Anton Oshurek <oshurekweb@gmail.com>
2023-02-01 23:27:15 +0100

    fix stroke-width and linecap bug

--- frontend/src/app/applications/weather-app/components/weather-search-locations/weather-search-locations.tsx
+++ frontend/src/app/applications/weather-app/components/weather-search-locations/weather-search-locations.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 //styles
 import './weather-search-locations.scss';
 
 const WeatherSearchLocations = (): JSX.Element => {
+
+	const [city, setCity] = useState("");
+  const [hint, setHint] = useState<any>([]);
+
+  const handleCityInput = (event: any) => {
+    setCity(event.target.value);
+    const cities = ["New York", "London", "Paris", "Berlin", "Tokyo"];
+    const cityHint = cities.filter((city) => city.toLowerCase().includes(event.target.value.toLowerCase()));
+    setHint(cityHint);
+  };
+
 	return (
 		<form className='weather-search-location'>
 			<label className='weather-search-location__label'>
 				<span className='weather-search-location__placeholder'>Search location</span>
-				<input className='weather-search-location__input' type="text" placeholder='Poznań' />
+				<input className='weather-search-location__input' type="text" placeholder='Poznań'
+					value={city}
+					onChange={handleCityInput}
+				/>
 			</label>
 
 			<button className='weather-search-location__submit' type='button'>
@@ -15,6 +31,14 @@ const WeatherSearchLocations = (): JSX.Element => {
 					<path fill="currentColor" d="M11 14h2v-3h3V9h-3V6h-2v3H8v2h3Zm1 8q-4.025-3.425-6.012-6.363Q4 12.7 4 10.2q0-3.75 2.413-5.975Q8.825 2 12 2t5.587 2.225Q20 6.45 20 10.2q0 2.5-1.987 5.437Q16.025 18.575 12 22Z"></path>
 				</svg>
 			</button>
+
+      {/* {hint.length > 0 && (
+        <ul>
+          {hint.map((city: any) => (
+            <li key={city}>{city}</li>
+          ))}
+        </ul>
+      )} */}
 		</form>
 	);
 };
