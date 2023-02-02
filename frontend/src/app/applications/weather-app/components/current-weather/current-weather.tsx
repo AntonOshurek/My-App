12f23a31ab786a4a884db02d3bf3e09881a8c927
Anton Oshurek <oshurekweb@gmail.com>
2023-02-02 14:09:52 +0100

    create functions for get date nad weekday

--- frontend/src/app/applications/weather-app/components/current-weather/current-weather.tsx
+++ frontend/src/app/applications/weather-app/components/current-weather/current-weather.tsx
@@ -11,18 +11,39 @@ const CurrentWeather = ({currentWeather}: ICurrentWeatherPropsType): JSX.Element
 
 	console.log(currentWeather);
 
+	function formatDate(dateStr: string | undefined) {
+		if(dateStr) {
+			const date = new Date(dateStr);
+			const options: {[value: string]: string} = { year: "numeric", month: "short", day: "numeric" };
+			return date.toLocaleDateString("en-US", options);
+		} else {
+			return '';
+		}
+	};
+
+	function getWeekday(dateStr: string | undefined) {
+
+		if(dateStr) {
+			const date = new Date(dateStr);
+			const options: {[value: string]: string} = { weekday: "long" };
+			return date.toLocaleDateString("en-US", options);
+		} else {
+			return '';
+		}
+	}
+
 	return (
 		<article className='current-weather'>
 			<h3 className='visually-hidden'>Weather for tooday</h3>
 			<div className='current-weather__wrap'>
 				<time className='current-weather__date' dateTime={currentWeather?.location.localtime}>
-					<span className='current-weather__date-text current-weather__date-text--day'>Tuesday</span>
-					<span className='current-weather__date-text'>15 Jan 2019</span>
+					<span className='current-weather__date-text current-weather__date-text--day'>{getWeekday(currentWeather?.location.localtime)}</span>
+					<span className='current-weather__date-text'>{formatDate(currentWeather?.location.localtime)}</span>
 				</time>
 
 				<p className='current-weather__city'>{currentWeather?.location.name}</p>
 
-				<span className='current-weather__image'>{currentWeather?.location.region}</span>
+				<img className='current-weather__image' src={currentWeather?.current.conditionIcon} alt={currentWeather?.current.condition}/>
 
 				<p className='current-weather__temperature'>{currentWeather?.current.temperatureC}°C</p>
 				<p className='current-weather__temperature-info'>{currentWeather?.current.condition}</p>
