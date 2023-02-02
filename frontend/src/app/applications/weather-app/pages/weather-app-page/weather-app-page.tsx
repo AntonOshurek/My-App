e7dee9060e435ed2696cb10c3b4e118130624af4
Anton Oshurek <oshurekweb@gmail.com>
2023-02-02 11:42:54 +0100

    refactoring app page

--- frontend/src/app/applications/weather-app/pages/weather-app-page/weather-app-page.tsx
+++ frontend/src/app/applications/weather-app/pages/weather-app-page/weather-app-page.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 //components
 import { CurrentWeather, WeatherControls } from '../../components/';
 //api
@@ -13,33 +14,48 @@ import './weather-app-page.scss';
 const WeatherAppPage = (): JSX.Element => {
 	const {location, day} = useParams();
 
-	if(location) {
-		const weatherConfiguration: IGetWeatherConfigurationType = {
-			days: 3,
-			city: location,
-			lang: 'ru',
-		};
+	const [currentLocation, setCurrentLocation] = useState<string>('');
 
-		weatherApi.getWeather(weatherConfiguration)
-		.then((response) => {
-			console.log(response);
-		})
-		.catch((error) => {
-			if(error.message.search('City error') >= 0) {
-				console.log('City error')
-			} else {
-				console.log('somthing wrong...')
-			}
-		});
-	} else {
-		locationService.getCurrentLocation()
-		.then((result) => {
-			console.log(result);
-		})
-		.catch(error => {
-			console.log(error);
-		});
-	}
+	useEffect(() => {
+		if(location || location?.length) {
+			setCurrentLocation(location);
+		} else {
+			locationService.getCurrentLocation()
+			.then((result) => {
+				setCurrentLocation(result);
+			})
+			.catch(error => {
+				console.log(error.message);
+				//if message === User denied Geolocation show error notification for client!
+				//user has denied access to location data - message
+			});
+		}
+	}, []);
+
+	useEffect(() => {
+		console.log(currentLocation);
+
+		if(currentLocation || currentLocation.length) {
+
+			console.log(currentLocation);
+
+			weatherApi.getWeather({
+				days: 3,
+				city: currentLocation,
+				lang: 'ru',
+			})
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				if(error.message.search('City error') >= 0) {
+					console.log('City error')
+				} else {
+					console.log('somthing wrong...')
+				}
+			});
+		};
+	}, [currentLocation]);
 
 	return (
 		<div className='weather-app-page'>
