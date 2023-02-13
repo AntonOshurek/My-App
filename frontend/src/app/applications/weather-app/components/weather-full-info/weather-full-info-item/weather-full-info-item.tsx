7310779894a524d00aec4a597c92651a012644a1
Anton Oshurek <oshurekweb@gmail.com>
2023-02-13 19:35:59 +0100

    styles correction for full info items

--- frontend/src/app/applications/weather-app/components/weather-full-info/weather-full-info-item/weather-full-info-item.tsx
+++ frontend/src/app/applications/weather-app/components/weather-full-info/weather-full-info-item/weather-full-info-item.tsx
@@ -19,39 +19,39 @@ const WeatherFullInfoItem = ({hourWeather}: IWeatherFullInfoItemPropsType): JSX.
 					<time dateTime={hourWeather.time}>{getTimeFromString(hourWeather.time)}</time>
 				</p>
 
-				<p>Chance of Rain: {hourWeather.chanceOfRain}</p>
-				<p>Chance of Snow: {hourWeather.chanceOfSnow}</p>
-				<p>Cloud: {hourWeather.cloud}</p>
-				<p>Condition: {hourWeather.condition.text}</p>
-				<p>Dewpoint C: {hourWeather.dewpointC}</p>
-				<p>Dewpoint F: {hourWeather.dewpointF}</p>
-				<p>Feelslike C: {hourWeather.feelslikeC}</p>
-				<p>Feelslike F: {hourWeather.feelslikeF}</p>
-				<p>Gust Kph: {hourWeather.gustKph}</p>
-				<p>Gust Mph: {hourWeather.gustMph}</p>
-				<p>Heatindex C: {hourWeather.heatindexC}</p>
-				<p>Heatindex F: {hourWeather.heatindexF}</p>
-				<p>Humidity: {hourWeather.humidity}</p>
-				<p>Is Day: {hourWeather.isDay ? 'Yes' : 'No'}</p>
-				<p>Precip In: {hourWeather.precipIn}</p>
-				<p>Precip Mm: {hourWeather.precipMm}</p>
-				<p>Pressure In: {hourWeather.pressureIn}</p>
-				<p>Pressure Mb: {hourWeather.pressureMb}</p>
-				<p>Temp C: {hourWeather.tempC}</p>
-				<p>Temp F: {hourWeather.tempF}</p>
-				<p>Time: {hourWeather.time}</p>
-				<p>Time Epoch: {hourWeather.timeEpoch}</p>
-				<p>UV: {hourWeather.uv}</p>
-				<p>Vis Km: {hourWeather.visKm}</p>
-				<p>Vis Miles: {hourWeather.visMiles}</p>
-				<p>Will it Rain: {hourWeather.willItRain ? 'Yes' : 'No'}</p>
-				<p>Will it Snow: {hourWeather.willItSnow ? 'Yes' : 'No'}</p>
-				<p>Wind Degree: {hourWeather.windDegree}</p>
-				<p>Wind Dir: {hourWeather.windDir}</p>
-				<p>Wind Kph: {hourWeather.windKph}</p>
-				<p>Wind Mph: {hourWeather.windMph}</p>
-				<p>Windchill C: {hourWeather.windchillC}</p>
-				<p>Windchill F: {hourWeather.windchillF}</p>
+				<p className='weather-full-info__text'>Chance of Rain: <span className='weather-full-infi__data'>{hourWeather.chanceOfRain}</span></p>
+				<p className='weather-full-info__text'>Chance of Snow: <span className='weather-full-infi__data'>{hourWeather.chanceOfSnow}</span></p>
+				<p className='weather-full-info__text'>Cloud: <span className='weather-full-infi__data'>{hourWeather.cloud}</span></p>
+				<p className='weather-full-info__text'>Condition: <span className='weather-full-infi__data'>{hourWeather.condition.text}</span></p>
+				<p className='weather-full-info__text'>Dewpoint C: <span className='weather-full-infi__data'>{hourWeather.dewpointC}</span></p>
+				<p className='weather-full-info__text'>Dewpoint F: <span className='weather-full-infi__data'>{hourWeather.dewpointF}</span></p>
+				<p className='weather-full-info__text'>Feelslike C: <span className='weather-full-infi__data'>{hourWeather.feelslikeC}</span></p>
+				<p className='weather-full-info__text'>Feelslike F: <span className='weather-full-infi__data'>{hourWeather.feelslikeF}</span></p>
+				<p className='weather-full-info__text'>Gust Kph: <span className='weather-full-infi__data'>{hourWeather.gustKph}</span></p>
+				<p className='weather-full-info__text'>Gust Mph: <span className='weather-full-infi__data'>{hourWeather.gustMph}</span></p>
+				<p className='weather-full-info__text'>Heatindex C: <span className='weather-full-infi__data'>{hourWeather.heatindexC}</span></p>
+				<p className='weather-full-info__text'>Heatindex F: <span className='weather-full-infi__data'>{hourWeather.heatindexF}</span></p>
+				<p className='weather-full-info__text'>Humidity: <span className='weather-full-infi__data'>{hourWeather.humidity}</span></p>
+				<p className='weather-full-info__text'>Is Day: <span className='weather-full-infi__data'>{hourWeather.isDay ? 'Yes' : 'No'}</span></p>
+				<p className='weather-full-info__text'>Precip In: <span className='weather-full-infi__data'>{hourWeather.precipIn}</span></p>
+				<p className='weather-full-info__text'>Precip Mm: <span className='weather-full-infi__data'>{hourWeather.precipMm}</span></p>
+				<p className='weather-full-info__text'>Pressure In: <span className='weather-full-infi__data'>{hourWeather.pressureIn}</span></p>
+				<p className='weather-full-info__text'>Pressure Mb: <span className='weather-full-infi__data'>{hourWeather.pressureMb}</span></p>
+				<p className='weather-full-info__text'>Temp C: <span className='weather-full-infi__data'>{hourWeather.tempC}</span></p>
+				<p className='weather-full-info__text'>Temp F: <span className='weather-full-infi__data'>{hourWeather.tempF}</span></p>
+				<p className='weather-full-info__text'>Time: <span className='weather-full-infi__data'>{hourWeather.time}</span></p>
+				<p className='weather-full-info__text'>Time Epoch: <span className='weather-full-infi__data'>{hourWeather.timeEpoch}</span></p>
+				<p className='weather-full-info__text'>UV: <span className='weather-full-infi__data'>{hourWeather.uv}</span></p>
+				<p className='weather-full-info__text'>Vis Km: <span className='weather-full-infi__data'>{hourWeather.visKm}</span></p>
+				<p className='weather-full-info__text'>Vis Miles: <span className='weather-full-infi__data'>{hourWeather.visMiles}</span></p>
+				<p className='weather-full-info__text'>Will it Rain: <span className='weather-full-infi__data'>{hourWeather.willItRain ? 'Yes' : 'No'}</span></p>
+				<p className='weather-full-info__text'>Will it Snow: <span className='weather-full-infi__data'>{hourWeather.willItSnow ? 'Yes' : 'No'}</span></p>
+				<p className='weather-full-info__text'>Wind Degree: <span className='weather-full-infi__data'>{hourWeather.windDegree}</span></p>
+				<p className='weather-full-info__text'>Wind Dir: <span className='weather-full-infi__data'>{hourWeather.windDir}</span></p>
+				<p className='weather-full-info__text'>Wind Kph: <span className='weather-full-infi__data'>{hourWeather.windKph}</span></p>
+				<p className='weather-full-info__text'>Wind Mph: <span className='weather-full-infi__data'>{hourWeather.windMph}</span></p>
+				<p className='weather-full-info__text'>Windchill C: <span className='weather-full-infi__data'>{hourWeather.windchillC}</span></p>
+				<p className='weather-full-info__text'>Windchill F: <span className='weather-full-infi__data'>{hourWeather.windchillF}</span></p>
 
 			</article>
 		</li>
