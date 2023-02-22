2aafa890b42c511842897d6a637177f9f2613b75
Anton Oshurek <oshurekweb@gmail.com>
2023-02-22 19:09:50 +0100

    create settings pages logick

---
+++ frontend/src/app/components/app-menu/menu-account-settings/menu-account-settings.tsx
@@ -0,0 +1,11 @@
+//styles
+import './menu-account-settings.scss';
+
+const MenuAccountSettings = (): JSX.Element => {
+	return (
+		<div className='menu-settings'>
+		</div>
+	);
+};
+
+export default MenuAccountSettings;
