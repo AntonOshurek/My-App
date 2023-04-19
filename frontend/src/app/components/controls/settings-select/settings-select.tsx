efe69265337876e91e0cefb45f264d2fcb8faa5b
Anton Oshurek <oshurekweb@gmail.com>
2023-04-19 20:11:53 +0200

    create components for settings items

---
+++ frontend/src/app/components/controls/settings-select/settings-select.tsx
@@ -0,0 +1,18 @@
+import './settings-select.scss';
+
+const SettingsSelect = (): JSX.Element => {
+	return (
+		<div className='settings-select'>
+			<label className='settings-select__label'>
+				Language
+				<select className='settings-select__select' name="language">
+					<option className='settings-select__option' value="eng">English</option>
+					<option className='settings-select__option' value="pol">Polish</option>
+					<option className='settings-select__option' value="rus">Russian</option>
+				</select>
+			</label>
+		</div>
+	);
+};
+
+export default SettingsSelect
